85643c91463f69b8265c21ed648256eba7e42212
Ruturaj-007 <pruturaj3003@gmail.com>
2025-07-09 21:03:23 +0530

    chore : add minimal comments to binary search code

--- src/binarySearch/binary_search.java
+++ src/binarySearch/binary_search.java
@@ -7,18 +7,18 @@ public class binary_search {
         int lo = 0, hi = len - 1, target = 4;
         boolean flag = false;
 
+        // Binary search loop
         while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
+            int mid = lo + (hi - lo) / 2;  // Avoid overflow
             if (arr[mid] == target) {
                 flag = true;
                 break;
-            } else if (arr[mid] < target) {
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
             }
+            else if (arr[mid] < target) lo = mid + 1;
+            else hi = mid - 1;
         }
 
+        // Output based on search result
         if (flag) System.out.println("Target found!");
         else System.out.println("Target not found!");
     }
