52f0d361f07281e6fb8ed19c0f3018521e3942e9
Ruturaj-007 <pruturaj3003@gmail.com>
2025-10-07 22:16:32 +0530

    feat: implement search in 2D array returning row and column indices of target

--- src/binarySearch/SearchIn2dArray.java
+++ src/binarySearch/SearchIn2dArray.java
@@ -13,6 +13,8 @@ public class SearchIn2dArray {
         int target = 34;
         int[] ans = searching(arr,target); // format of return value {row, col}
         System.out.println(Arrays.toString(ans));
+        System.out.println("Max element in array : " + maxElement(arr));
+        System.out.println("Max element in array : " + max(arr));
     }
     static int[] searching(int[][] arr, int target) {
         for (int row = 0; row < arr.length ; row++) {
@@ -23,5 +25,29 @@ public class SearchIn2dArray {
             }
         }
         return new int[]{-1, -1};
+    }
+
+    static int maxElement(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int row = 0; row < arr.length ; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if(arr[row][col] > max) {
+                    max = arr[row][col];
+                }
+            }
+        }
+        return max;
+    }
+
+    static int max(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int[] ints : arr) {
+            for (int element : ints) {
+                if (element > max) {
+                    max = element;
+                }
+            }
+        }
+        return max;
     }
 }
