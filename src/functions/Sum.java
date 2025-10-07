88d093dff15b352d6f2da985a7c3e7f9b358c8b2
Ruturaj-007 <pruturaj3003@gmail.com>
2025-10-07 18:55:50 +0530

    feat:Add Sum of two numbers

--- src/functions/Sum.java
+++ src/functions/Sum.java
@@ -1,4 +1,20 @@
 package functions;
 
+import java.util.Scanner;
+
 public class Sum {
+    public static void main(String[] args) {
+        sum();
+        sum();
+    }
+    static void sum() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number 1 : ");
+        int num1 = in.nextInt();
+        System.out.println("Enter a number 2 : ");
+        int num2 = in.nextInt();
+        int sum  = num2+num1;
+        System.out.println("Sum of 2 numbers is " + sum);
+
+    }
 }
