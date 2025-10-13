1a00bd2302307a74fc07d496e4dfac0f0a629622
Ruturaj-007 <pruturaj3003@gmail.com>
2025-10-13 18:25:59 +0530

    fix:add case for (-ve) and (0) number

--- src/binarySearch/EvenDigits.java
+++ src/binarySearch/EvenDigits.java
@@ -23,6 +23,8 @@ public class EvenDigits {
         return false;
     }
     static int digits(int num) {
+        if (num < 0) num*=-1;
+        if (num==0) return 1;
         int count = 0;
         while(num>0) {
             count++;
